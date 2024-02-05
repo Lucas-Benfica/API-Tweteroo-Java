7826fe64f729ce048474413ac59fab41cd1051a9
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2024-02-05 14:24:10 -0300

    feat: tables models

---
+++ src/main/java/com/tweteroo/api/models/UserModel.java
@@ -0,0 +1,27 @@
+package com.tweteroo.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @Column(nullable = false)
+    private String avatar;
+    @Column(nullable = false, length = 100, unique = true)
+    private String username;
+}
